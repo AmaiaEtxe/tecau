552355112bc289fe0992797693ee22daf64ed6f4
AmaiaEtxe <115586373+AmaiaEtxe@users.noreply.github.com>
2023-06-02 18:59:22 +0200

    Update entrenamiento_modelo.py

--- entrenamiento_modelo.py
+++ entrenamiento_modelo.py
@@ -18,37 +18,41 @@ peopleList = [item for item in peopleList if os.path.isdir(os.path.join(dataPath
 
 print('Lista de personas: ', peopleList)
 
+#Arrays para etiquetado de imagenes para el entrenamiento
 labels = []
 facesData = []
 label = 0
 
+#Lectura de rostros y etiquetado
 for nameDir in peopleList:
     personPath = dataPath + '/' + nameDir
     print('Leyendo las imágenes')
+    #Filtrado de imagenes de rostros detectados en el directorio 'personPath'
     lista_caras_registradas = [archivo for archivo in os.listdir(personPath) if archivo.endswith('.jpg')]
-
+    
+    #Barra de progreso
     with tqdm(total=len(lista_caras_registradas), desc="Progreso de lectura y clasificación de imágenes") as pbar:
+        #Recorremos cada una de los rostros del directorio
         for fileName in lista_caras_registradas:
+            #Almacenar la etiqueta para la imagen en el array
             labels.append(label)
+            #Almacenar la imagen en el array
             facesData.append(cv2.imread(personPath+'/'+fileName,0))
             pbar.update(1)  # Actualizar el progreso de la barra
         label = label + 1
         
-# Se selecciona el método para entrenar el reconocedor
-#face_recognizer = cv2.face.EigenFaceRecognizer_create()
-#face_recognizer = cv2.face.FisherFaceRecognizer_create()
+# Se selecciona el método LBPH para entrenar el reconocedor
 face_recognizer = cv2.face.LBPHFaceRecognizer_create()
 
 # Se entrena el modelo reconocedor de rostros
 print("Entrenando...")
 face_recognizer.train(facesData, np.array(labels))
 
+#Creamos carpeta para guardar el modelo entrenado si no existiera
 if not os.path.exists('modelos'):
     os.makedirs('modelos')
     print('Carpeta creada: ', 'modelos')
 
-# Se almacena el modelo obtenido
-#face_recognizer.write('modelos/modeloEigenFace.xml')
-#face_recognizer.write('modelos/modeloFisherFace.xml')
+# Se almacena el modelo obtenido en formato XML
 face_recognizer.write('modelos/modeloLBPHFace.xml')
 print("Modelo almacenado")
